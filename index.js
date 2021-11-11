c4df30c0d284c921e2015171ecabfadc8653c0db
Daniella Fernandes <fernandesdaniella@hotmail.com>
2021-11-11 13:37:36 +0000

    Update counter

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const moveForward = (player) => {
   const car = document.querySelector(`#player-${player} .active`);
   const start1 = document.getElementById('start1');
   const start2 = document.getElementById('start2');
-  const middle = document.querySelector('.middle-active');
+  const middle = document.querySelectorAll('.middle');
   if(car.nextElementSibling) {
     car.nextElementSibling.classList.add('active');
     car.classList.remove('active');
   } else {
     alert(`Player ${player} wins! Play again?`);
-    start1.classList.add('active');
-    start2.classList.add('active');
-    middle.classList.remove('active');
+    window.location.reload();
+    // start1.classList.add('active');
+    // start2.classList.add('active');
+    // middle.classList.remove('active');
   }
 };
 
@@ -23,3 +24,13 @@ const moveCars = (event) => {
 };
 
 document.addEventListener("keyup", moveCars);
+
+
+let counterDisplayElem = document.querySelectorAll('.counter-display');
+let count = 0;
+const updateDisplay = () => {
+  counterDisplayElem.forEach(element => {
+  element.innerHTML = count;
+  })
+};
+updateDisplay();
